49454d3d11e1cf3c2f9f6f400883dc112ae191a0
Gak1o <gakiotoni@gmail.com>
2025-02-25 14:58:49 +0300

    Initialized repository

---
+++ assignment.ts
@@ -0,0 +1,16 @@
+interface Todo {
+    taskname: string;
+    finished: boolean;
+}
+
+const todos: Todo[] = [];
+
+const addTodo = () =>{
+    for(let i:number = 0; i<9; i++){
+        todos.push(...todos, {taskname: "Hello", finished: true});
+    }
+}
+
+addTodo();
+
+console.log(todos);
